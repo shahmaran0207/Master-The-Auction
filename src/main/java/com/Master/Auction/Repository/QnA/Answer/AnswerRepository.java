03790ec6231cfd5f85fc0b3bf1b1c891e019a7da
shahmaran0207 <shahmaran0207@naver.com>
2025-02-11 09:08:04 +0900

    answer try - 1

---
+++ src/main/java/com/Master/Auction/Repository/QnA/Answer/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.Master.Auction.Repository.QnA.Answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.Master.Auction.Entity.QnA.Answer.AnswerEntity;
+
+public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
+
+}
---
+++ src/main/java/com/Master/Auction/Service/QnA/Answer/AnswerService.java
@@ -0,0 +1,34 @@
+package com.Master.Auction.Service.QnA.Answer;
+
+import com.Master.Auction.Repository.QnA.Question.QuestionRepository;
+import com.Master.Auction.Repository.QnA.Answer.AnswerRepository;
+import com.Master.Auction.Repository.Member.MemberRepository;
+import com.Master.Auction.Entity.QnA.Question.QuestionEntity;
+import com.Master.Auction.Entity.QnA.Answer.AnswerEntity;
+import com.Master.Auction.Entity.Member.MemberEntity;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final MemberRepository memberRepository;
+    private final AnswerRepository answerRepository;
+    private final QuestionRepository questionRepository;
+
+    public void save(Long id, String contents, QuestionEntity question) throws IOException {
+        MemberEntity memberEntity = memberRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid member ID: " + id));
+
+        AnswerEntity answerEntity = AnswerEntity.toSaveEntity(memberEntity, contents, question);
+        answerRepository.save(answerEntity);
+
+        QuestionEntity existingQuestion = questionRepository.findById(question.getId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid question ID: " + question.getId()));
+
+        existingQuestion.setAnswerStatus("answered");
+        questionRepository.save(existingQuestion);
+    }
+
+}
