e4a86390b4743dbd3c7c28e2503e01d4b0e94ae3
shahmaran0207 <shahmaran0207@naver.com>
2025-03-06 10:06:41 +0900

    Auction filter

--- src/main/java/com/Master/Auction/Repository/Auction/AuctionRepository.java
+++ src/main/java/com/Master/Auction/Repository/Auction/AuctionRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import com.Master.Auction.Entity.Auction.AuctionEntity;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -14,4 +16,8 @@ public interface AuctionRepository extends JpaRepository<AuctionEntity, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update AuctionEntity a set a.AuctionHits = a.AuctionHits + 1 where a.id = :id")
     void updateHits(@Param("id") Long id);
+
+    Page<AuctionEntity> findByAuctionStatus(String auctionStatus, Pageable pageable);
+
+
 }
